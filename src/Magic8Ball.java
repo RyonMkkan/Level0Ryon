965fefac0c62a9258ff7aefee1158a3c22e6dadd
Ryon <league@Mac-mini-3.local>
2016-12-15 16:53:14 -0800

    lv

--- src/Magic8Ball.java
+++ src/Magic8Ball.java
@@ -15,19 +15,28 @@ JOptionPane.showMessageDialog(null, random);
 	// 4. Get the user to enter a question for the 8 ball
 JOptionPane.showInputDialog("whats your question?", "");
 	// 5. If the random number is 0
-
+if (random==0) {
+	JOptionPane.showMessageDialog(null, "jes");
+}
 	// -- tell the user "Yes"
 
 	// 6. If the random number is 1
-
+if (random==1) {
+	JOptionPane.showMessageDialog(null, "no");
+}
 	// -- tell the user "No"
 
 	// 7. If the random number is 2
+if (random==2) {
+	JOptionPane.showMessageDialog(null, "ask google");
+}
 
 	// -- tell the user "Maybe you should ask Google?"
 
 	// 8. If the random number is 3
-
+if (random==3) {
+	JOptionPane.showMessageDialog(null, "monkey");
+}
 	// -- write your own answer
 }
 }
